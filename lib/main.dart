c3e3ac3395feb978cd6b4eb955b0325da71c1601
Jabari Neal-Jackson <jabarijnj@gmail.com>
2024-12-20 13:29:51 -0500

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weatherapp/Pages/weather_pages.dart';
+import 'package:weatherapp/Pages/weather_page.dart';
 
 void main() {
   runApp(const MyApp());
